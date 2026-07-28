ab34eba8efd2b51c74b1a76ffcb114e0097c027b
Ezyyo <ezyyo2025@gmail.com>
2026-07-28 18:49:15 +0300

    Add initial index.js for EzyYo Marketing service

---
+++ src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict';
+
+function main() {
+  console.log('EzyYo Marketing starter — service is up');
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = { main };
